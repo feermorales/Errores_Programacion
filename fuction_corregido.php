61639f8e99950ace87fe4572ba061965845d310b
BereniceSantiago <bere_082598@outlook.com>
2020-04-03 15:54:37 -0600

    Errores fatales

---
+++ fuction_corregido.php
@@ -0,0 +1,12 @@
+<?php
+
+function unaFuncion() {
+    return true;
+}
+//La siguiente funcion no existe y se genera un error Fatal
+echo "Practica Seguridad Informatica";
+function otraFuncion() {
+    $name = "Bere";
+    echo 'Tu eres ' . $nombre;
+}
+?>
---
+++ fuction_error.php
@@ -0,0 +1,7 @@
+<?php
+function unaFuncion() {
+    return true;
+}
+//La siguiente funcion no existe y se genera un error Fatal
+otraFuncion();
+?>
