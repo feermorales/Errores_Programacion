42f7162c2c4b1097c5997d899c7bee54244f83ca
feermorales <feernando_morales@outlook.es>
2020-04-03 20:56:11 +0000

    Codigo en php del error BREAK

---
+++ break_corregido.php
@@ -0,0 +1,14 @@
+<?php
+$prueba= 3;
+switch($prueba){
+     case 1: echo 'algo';
+                break;
+     case 2: echo 'algo mas';
+                break;
+     case 3: echo 'mucho mas';
+                break;
+     case 4: echo 'bastante';
+                break;
+     default: echo 'opcion por defecto si ninguno de las opciones se cumple';
+}
+?>
---
+++ break_error.php
@@ -0,0 +1,9 @@
+<?php
+$prueba= 2;
+switch($prueba){
+    case 1: echo 'algo';
+    case 2: echo 'algo mas';
+    case 3: echo 'mucho mas';    
+    case 4: echo 'bastante';
+}
+?>
